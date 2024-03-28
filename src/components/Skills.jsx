20fd31abab1f38b1508174f799a0c77324d364df
VaibhavDhamanage <vaibhavdhamanage>
2024-03-28 18:47:26 +0530

    add skill section using content obj data

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,58 @@
+// import content
+import { createElement, useState } from "react";
+import { content } from "../Content";
+// import modal package
+import Modal from "react-modal";
+
 const Skills = () => {
-  return <div>Skills</div>;
+  const { skills } = content;
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <section className="min-h-fit bg-bg_light_primary" id="skills">
+      <div className="md:container px-5  py-14">
+        <h2 className="title">{skills.title}</h2>
+        <h4 className="subtitle">{skills.subtitle}</h4>
+        <br />
+        <div className="flex flex-wrap gap-4 justify-center">
+          {skills.skills_content.map((skill, i) => (
+            <div
+              key={i}
+              className="bg-white sm:cursor-pointer 
+               relative group w-full flex items-center
+                gap-5 p-5 max-w-sm rounded-xl border-2 border-slate-200"
+            >
+              <div>
+                <img
+                  src={skill.logo}
+                  alt="..."
+                  className="w-10 group-hover:scale-125 duration-200"
+                />
+              </div>
+              <div>
+                <h6>{skill.name}</h6>
+                <p className="italic">{skill.para}</p>
+                <div
+                  onClick={() => {
+                    setSelectSkill(skill);
+                    openModal();
+                  }}
+                  className="text-xl absolute top-3 right-3"
+                ></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
