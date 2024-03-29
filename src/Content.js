f2f39fdc1d160e5e90e76210ed9fe045f46b403c
VaibhavDhamanage <vaibhavdhamanage>
2024-03-29 18:58:30 +0530

    clean files

--- src/Content.js
+++ src/Content.js
@@ -12,27 +12,16 @@ import bootstrap from "./assets/images/Skills/bootstrap.png";
 import firebase from "./assets/images/Skills/firebase.png";
 import github from "./assets/images/Skills/github.png";
 
-import services_logo1 from "./assets/images/Services/logo1.png";
-import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
-
 import project1 from "./assets/images/projects/blogger.png";
 import project2 from "./assets/images/projects/mail-box.png";
 import project3 from "./assets/images/projects/ecommerce.png";
 import project4 from "./assets/images/projects/expense-tracker.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
-
-import Hireme_person from "./assets/images/Hireme/person.png";
 // import Hireme_person2 from "./assets/images/Hireme/person2.png";
 
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
 import { MdArrowForward, MdCall } from "react-icons/md";
-import { BsInstagram } from "react-icons/bs";
 import { TbSmartHome } from "react-icons/tb";
 import { BiUser } from "react-icons/bi";
 import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
